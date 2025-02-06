5aa9071afda0c9f4de1039db01d928d4f087eb7f
ryestew <rob@stupay.com>
2025-02-06 10:10:58 -0500

    remove git workflow

--- contracts/2_Owner.sol
+++ contracts/2_Owner.sol
@@ -18,7 +18,7 @@ contract Owner {
     // modifier to check if caller is owner
     modifier isOwner() {
         // If the first argument of 'require' evaluates to 'false', execution terminates and all
-        // changes to the state and to Ether balances are reverted. asdfaf adsfadsf
+        // changes to the state and to Ether balances are reverted. asdfaf adsfadsfasdf
         // This used to consume all gas in old EVM versions, but not anymore.
         // It is often a good idea to use 'require' to check if functions are called correctly.
         // As a second argument, you can also provide an explanation about what went wrong.
